bf88a4dc9a735b97bc7dfe9abd1d6aeb213298f2
Justin Reynolds <reynolds.justinmichael@gmail.com>
2023-10-26 16:14:25 +0200

    Auth

--- src/App.jsx
+++ src/App.jsx
@@ -2,50 +2,28 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
-import { ClerkProvider, SignedIn, SignedOut, UserButton, useUser, RedirectToSignIn, SignIn, } from "@clerk/clerk-react";
+import { ClerkProvider, SignedIn, SignedOut, UserButton, useUser, RedirectToSignIn, SignIn } from "@clerk/clerk-react";
+import { dark } from '@clerk/themes';
 import AppLayout from './Components/Layout/Layout';
-import './App.css'
+import './App.css';
 
 function App() {
-
   if (!import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY) {
-    throw new Error("Missing Publishable Key")
+    throw new Error("Missing Publishable Key");
   }
+
   const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY;
-  return (
-    <ClerkProvider publishableKey={clerkPubKey}
-      appearance={{
-        elements: {
-          footer: "hidden",
-        },
-      }}>
-      <SignedIn>
-        <RotorRisk />
-      </SignedIn>
-      <SignedOut>
-        <RedirectToSignIn />
-        {/* <SignIn /> */}
-      </SignedOut>
-    </ClerkProvider>
-  );
-}
-function RotorRisk() {
 
   const [lightMode, setLightMode] = useState(false);
-  const [loading, setLoading] = useState(true); // New loading state
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const existingPreference = localStorage.getItem('lightMode');
     if (existingPreference) {
       setLightMode(existingPreference === 'true');
     }
-    setLoading(false); // Mark loading as false when the effect completes
+    setLoading(false);
   }, []);
-
-  // Wait until loading is false to render the content
-  if (loading) {
-    return null; // Or a loading indicator
-  }
 
   const lightPalette = {
     mode: 'light',
@@ -59,7 +37,7 @@ function RotorRisk() {
       default: '#FAF1E4',
       paper: '#fff'
     },
-  }
+  };
   const darkPalette = {
     mode: 'dark',
     primary: {
@@ -72,35 +50,36 @@ function RotorRisk() {
     text: {
       primary: '#fff',
     },
-  }
+  };
   const theme = createTheme({
     palette: {
       mode: lightMode ? 'light' : 'dark',
-      ... (lightMode ? lightPalette : darkPalette),
+      ...(lightMode ? lightPalette : darkPalette),
     },
   });
-
   const handleLightModeToggle = () => {
     const newLightMode = !lightMode;
     setLightMode(newLightMode);
     localStorage.setItem('lightMode', newLightMode.toString());
   };
+
   return (
-    < ThemeProvider theme={theme} >
-      <CssBaseline />
-      <AppLayout lightMode={lightMode} handleLightModeToggle={handleLightModeToggle} />
-    </ThemeProvider >
-  )
-}
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ClerkProvider publishableKey={clerkPubKey}>
+    <ClerkProvider publishableKey={clerkPubKey} appearance={{ baseTheme: lightMode ? '' : dark, elements: { footer: "hidden" } }}>
       <SignedIn>
-        <App />
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <AppLayout lightMode={lightMode} handleLightModeToggle={handleLightModeToggle} />
+        </ThemeProvider>
       </SignedIn>
       <SignedOut>
         <RedirectToSignIn />
       </SignedOut>
     </ClerkProvider>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
   </React.StrictMode>
 );
